945615cb1395d8ed7b3d0c2d5e3662757824fa53
Luiz Lopes <luiz@wickeddeveloper.com>
2018-05-23 17:27:38 -0600

    Better setup for hypothetical scenario

--- index.js
+++ index.js
@@ -1,21 +1,42 @@
+const crypto = require('crypto');
 const Blockchain = require('./src/blockchain');
 const Transaction = require('./src/transaction');
+
+// Hypothetical interaction between nodes on the network
+
+// Setting up wallet addresses using Elliptic Curve Diffie-Hellman (ECDH)
+const miner = crypto.createECDH('secp256k1');
+miner.generateKeys();
+const minerAddress = miner.getPublicKey('hex');
+const alice = crypto.createECDH('secp256k1');
+alice.generateKeys();
+const aliceAddress = alice.getPublicKey('hex');
+const bob = crypto.createECDH('secp256k1');
+bob.generateKeys();
+const bobAddress = bob.getPublicKey('hex');
 
 /**
  * @var {Blockchain}
  */
 const bsdCoin = new Blockchain();
 
-bsdCoin.createTransaction(new Transaction('address1', 'address2', 100));
-bsdCoin.createTransaction(new Transaction('address2', 'address1', 50));
+// A miner joins the network
+bsdCoin.minePendingTransactions(minerAddress);
 
-bsdCoin.minePendingTransactions('miner-address');
-bsdCoin.minePendingTransactions('miner-address');
+// miner pays bob for some widget
+bsdCoin.createTransaction(new Transaction(minerAddress, bobAddress, 100));
 
-/**
- * @var {String} minerBalance The balance of the miner-address
- */
-const minerBalance = bsdCoin.getBalanceOfAddress('miner-address');
+// bob pays alice for some service
+bsdCoin.createTransaction(new Transaction(bobAddress, aliceAddress, 50));
 
-console.log('Balance of miner-address  is', minerBalance);
-console.log('Blockchain', JSON.stringify(bsdCoin, null, 2));
+// miner attempts to solve puzzle again
+bsdCoin.minePendingTransactions(minerAddress);
+
+const minerBalance = bsdCoin.getBalanceOfAddress(minerAddress);
+const bobBalance = bsdCoin.getBalanceOfAddress(bobAddress);
+const aliceBalance = bsdCoin.getBalanceOfAddress(aliceAddress);
+
+console.log('Balance of miner\'s wallet is', minerBalance);
+console.log('Balance of bob\'s wallet is', bobBalance);
+console.log('Balance of alice\'s wallet is', aliceBalance);
+// console.log('Blockchain', JSON.stringify(bsdCoin, null, 2));
